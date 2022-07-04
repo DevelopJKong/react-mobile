6e0738358c013703bf2a8d0f27f8760e39371b4b
커피볶는정콩 <61773021+DevelopJKong@users.noreply.github.com>
2022-07-04 17:07:54 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserView, MobileView, isBrowser,deviceDetect} from "react-device-detect";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log(isBrowser);
+    console.log(deviceDetect());
+    return (
+        <>
+            <BrowserView>
+                <p>this is browserview</p>
+            </BrowserView>
+            <MobileView>
+                <p>this is mobileview</p>
+            </MobileView>
+        </>
+    );
 }
 
 export default App;
